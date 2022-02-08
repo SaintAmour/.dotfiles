93ed8ed045d4a3929dd3d3f0c7750a2c18038f0b
Brandon St. Amour <61098210+SaintAmour@users.noreply.github.com>
2022-02-07 21:09:53 -0500

    Rename repo from .dotfiles to dotfiles

--- symlink.sh
+++ symlink.sh
@@ -1,21 +1,21 @@
 # Symlink .gitconfig
 rm -i "$HOME/.gitconfig"
-ln -s "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
 
 # Symlink .zshrc
 rm -i "$HOME/.zshrc"
-ln -s "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
+ln -s "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
 
 # Symlink init.vim
 # Check if $HOME/.config/nvim exists
 if [ -d "$HOME/.config/nvim" ]; then
     rm -df "$HOME/.config/nvim"
 fi
-ln -s "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
+ln -s "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 
 # Symlink i3
 # Check if $HOME/.config/i3 exists
 if [ -d "$HOME/.config/i3" ]; then
     rm -df "$HOME/.config/i3"
 fi
-ln -s "$HOME/.dotfiles/i3" "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config/i3"
