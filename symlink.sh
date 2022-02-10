ccc60327be892e6e600042e8fc41db73fa33d6e0
Brandon St. Amour <61098210+SaintAmour@users.noreply.github.com>
2022-02-10 11:15:27 -0500

    i3 - add polybar

--- symlink.sh
+++ symlink.sh
@@ -9,13 +9,20 @@ ln -s "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
 # Symlink init.vim
 # Check if $HOME/.config/nvim exists
 if [ -d "$HOME/.config/nvim" ]; then
-    rm -df "$HOME/.config/nvim"
+    rm -drf "$HOME/.config/nvim"
 fi
 ln -s "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 
 # Symlink i3
 # Check if $HOME/.config/i3 exists
 if [ -d "$HOME/.config/i3" ]; then
-    rm -df "$HOME/.config/i3"
+    rm -drf "$HOME/.config/i3"
 fi
 ln -s "$HOME/dotfiles/i3" "$HOME/.config/i3"
+
+# Symlink polybar
+# Check if $HOME/.config/polybar exists
+if [ -d "$HOME/.config/polybar" ]; then
+    rm -drf "$HOME/.config/polybar"
+fi
+ln -s "$HOME/dotfiles/polybar" "$HOME/.config/polybar"
